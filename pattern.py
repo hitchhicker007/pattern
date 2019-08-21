e78993c0fcab4760a0fdcfea1562dd12089ffcfc
hitchhicker007 <hitchhicker8401@hotmail.com>
2019-08-21 05:54:54 -0400

    pattern

---
+++ pattern.py
@@ -0,0 +1,12 @@
+import turtle
+t = turtle.Turtle()
+sc=turtle.Screen()
+sc.title("Hitch Hicker")
+sc.bgcolor("black")
+t.hideturtle()
+list = ["purple","green","blue","red","orange"]
+for i in range(200):
+    t.color(list[i%5])
+    t.pensize(i/10+1)
+    t.forward(i)
+    t.left(59)
